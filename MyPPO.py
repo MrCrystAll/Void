f6fdc82dfab6e97765d43255ff1c1692772c6366
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2023-06-25 11:59:18 +0200

    Fixed actions not being rewarded

--- MyPPO.py
+++ MyPPO.py
@@ -172,7 +172,26 @@ class DynamicOnPolicyAlgorithm(BaseAlgorithm):
                 obs_tensor = obs_as_tensor(self._last_obs, self.device)
                 actions, values, log_probs = self.policy(obs_tensor)
             actions = actions.cpu().numpy()
-            actions = np.concatenate((actions, np.zeros(shape=(self.env.num_envs - actions.shape[0], 8))))
+
+            match = self.env.get_attr("_match")
+            all_matches = []
+            nb_agents = match[0].agents
+            i = match[0].team_size * 2 if match[0].spawn_opponents else match[0].team_size
+
+
+            if i == len(match) and nb_agents != actions.shape[0]:
+                actions[nb_agents:] = np.zeros(shape=(actions.shape[0] - nb_agents, 8))
+
+            while i < len(match):
+                actions[nb_agents:i] = np.zeros(shape=(i - nb_agents, 8))
+                nb_agents = i + match[i].agents
+                i += match[i].team_size * 2 if match[i].spawn_opponents else match[i].team_size
+
+                if i == len(match) and nb_agents != actions.shape[0]:
+                    actions[nb_agents:] = np.zeros(shape=(actions.shape[0] - nb_agents, 8))
+
+            self._last_obs = np.concatenate((self._last_obs, np.zeros(
+                shape=(self.env.num_envs - self._last_obs.shape[0], self.env.observation_space.shape[0]))))
             self._last_obs = np.concatenate((self._last_obs, np.zeros(
                 shape=(self.env.num_envs - self._last_obs.shape[0], self.env.observation_space.shape[0]))))
 
